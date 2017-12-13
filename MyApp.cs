e02fb18359bd6e018e946806da1da8e8c3d9b0fb
kakisoft <kakisttab@gmail.com>
2017-12-14 01:09:55 +0900

    edit

--- MyApp.cs
+++ MyApp.cs
@@ -69,6 +69,19 @@ class MyApp {
     //==============================
     //          文字列
     //==============================
+    Console.WriteLine("hello " + "world");
 
+    // \n, \t
+    Console.WriteLine("hell\no wo\trld");
+
+    var name = "taguchi";
+    var score = 52.3;
+
+    Console.WriteLine(string.Format("{0} [{1}]", name, score)); // taguchi [52.3]
+
+    Console.WriteLine($"{name} [{score}]");
+    Console.WriteLine($"{name, -10} [{score, 10}]");
+    Console.WriteLine($"{name, -10} [{score, 10:0.00}]");
+    Console.WriteLine($"{name, -10} [{score + 25, 10:0.00}]");
   }
 }
