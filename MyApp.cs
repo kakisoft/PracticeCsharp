fe5d6f50608c6abc3b8386fe73bd6e3832a7fe10
kakisoft <kakisttab@gmail.com>
2017-12-14 22:22:02 +0900

    edit

--- MyApp.cs
+++ MyApp.cs
@@ -1,9 +1,22 @@
 using System;
+
+class MyApp {
+
+  static void SayHi1() {
+    Console.WriteLine("hi!");
+  }
+  static string SayHi2() {
+    return "hi!";
+  }
+  static string SayHi3() => "hi!";
+
+  static void SayHi4(string name, int age = 23) {
+    Console.WriteLine($"hi! {name} ({age})");
+  }
+
   /*
     comment
   */
-class MyApp {
-
   static void Main() {
 
     //変数
@@ -25,7 +38,7 @@ class MyApp {
 
     // 整数値
     // byte, short, int, long
-    int i = 100;
+    int i1 = 100;
 
     // 浮動小数点数
     // float, double
@@ -71,17 +84,126 @@ class MyApp {
     //==============================
     Console.WriteLine("hello " + "world");
 
-    // \n, \t
+    // \n, \t    改行、タブ
     Console.WriteLine("hell\no wo\trld");
 
-    var name = "taguchi";
-    var score = 52.3;
+    var name1 = "taguchi";
+    var score1 = 52.3;
 
-    Console.WriteLine(string.Format("{0} [{1}]", name, score)); // taguchi [52.3]
+    Console.WriteLine(string.Format("{0} [{1}]", name1, score1)); // taguchi [52.3]
 
-    Console.WriteLine($"{name} [{score}]");
-    Console.WriteLine($"{name, -10} [{score, 10}]");
-    Console.WriteLine($"{name, -10} [{score, 10:0.00}]");
-    Console.WriteLine($"{name, -10} [{score + 25, 10:0.00}]");
+    Console.WriteLine($"{name1} [{score1}]");
+    Console.WriteLine($"{name1, -10} [{score1, 10}]");
+    Console.WriteLine($"{name1, -10} [{score1, 10:0.00}]");
+    Console.WriteLine($"{name1, -10} [{score1 + 25, 10:0.00}]");
+
+    //==============================
+    //          if
+    //==============================
+    // var score2 = int.Parse(Console.ReadLine());
+    var score2 = int.Parse("90");
+
+    // > >= < <= == !=
+    if (score2 > 80) {
+      Console.WriteLine("Great!");
+    } else if (score2 > 60) {
+      Console.WriteLine("Good!");
+    } else {
+      Console.WriteLine("so so ...!");
+    }
+
+    Console.WriteLine((score2 > 80) ? "Great" : "so so ...");
+
+    //==============================
+    //         switch
+    //==============================
+    // var signal = Console.ReadLine();
+    var signal = "red";
+
+    switch(signal) {
+      case "red":
+        Console.WriteLine("Stop!");
+        break;
+      case "blue":
+      case "green":
+        Console.WriteLine("Go!");
+        break;
+      case "yellow":
+        Console.WriteLine("Caution!");
+        break;
+      default:
+        Console.WriteLine("wrong signal!");
+        break;
+    }
+
+    //==============================
+    //         while
+    //==============================
+    // var i2 = 0;
+    var i2 = 100;
+
+    while (i2 < 10) {
+      Console.WriteLine($"loop:{i2}");
+      i2++;
+    }
+
+    do {
+      Console.WriteLine($"loop2:{i2}");
+      i2++;
+    } while (i2 < 10);
+
+    //==============================
+    //         for
+    //==============================
+    // continue それ以降の処理を中止して次のループへ
+    // break ループ自体を抜ける
+
+    for (int i = 0; i < 10; i++) {
+      if (i == 3) {
+        continue;
+      }
+      if (i == 5) {
+        break;
+      }
+      Console.WriteLine(i);
+    }
+
+    //==============================
+    //         配列
+    //==============================
+    // 配列
+    int[] scoresA1 = new int[3];
+    scoresA1[0] = 10;
+    scoresA1[1] = 30;
+    scoresA1[2] = 20;
+    int[] scoresA2 = new int[] {10, 30, 20};
+    int[] scoresA3 = {10, 30, 20};
+    var scoresA4 = new[] {10, 30, 20};
+
+    scoresA4[1] = 40;
+    Console.WriteLine(scoresA4[1]);
+
+    //==============================
+    //         foreach
+    //==============================
+    var scoresB1 = new[] {10, 30, 20};
+
+    // for (int i = 0; i < scores.Length; i++) {
+    //   Console.WriteLine(scores[i]);
+    // }
+
+    foreach (int score in scoresB1) {
+      Console.WriteLine(score);
+    }
+
+    //==============================
+    //         メソッド
+    //==============================
+    SayHi1();
+    Console.WriteLine(SayHi2());
+    Console.WriteLine(SayHi3());
+    SayHi4("Tom", 30); // tom 30
+    SayHi4("Bob"); // bob 23
+    SayHi4(age: 26, name: "Steve"); // steve 26
   }
 }
