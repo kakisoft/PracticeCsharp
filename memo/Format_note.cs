52c32c329aa1ac9fc756c920780b22c190d450a6
kakisoft <kakisoft@gmail.com>
2018-04-09 18:57:49 +0900

    Update Format_note.cs

--- memo/Format_note.cs
+++ memo/Format_note.cs
@@ -7,3 +7,8 @@ sampleInt01.ToString("#,0")  // 1,000
 sampleInt01.ToString("C")    // \1,000
 
 (12345).ToString("D8")       // 00012345
+
+  
+  
+int a1 = 20180601;
+string b1 = a1.ToString("0000/00/00"); // "2018/06/21"
